df5096eac95a0b397b10411e5d32407590a2cf29
juhyang <ljh28891004@gmail.com>
2021-11-16 21:45:07 +0900

    Material coponent

--- week 2-1-Layouts in JetpackCompose/week2_1/app/src/main/java/com/example/week2_1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/week2_1/app/src/main/java/com/example/week2_1/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
@@ -32,6 +34,42 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Week2Codelab() {
+    Scaffold(topBar = {
+        TopAppBar(title = {
+            Text(text = "Week2Codelab")
+        },
+            actions = {
+                IconButton(onClick = { /* do something */ }) {
+                    Icon(Icons.Filled.Favorite, contentDescription = null)
+                }
+            })
+    })
+    { innerPadding ->
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(8.dp)) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Week2_1Theme {
+        Week2Codelab()
     }
 }
 
@@ -75,9 +113,3 @@ fun photographerCardPreview() {
 }
 
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Week2_1Theme {
-    }
-}
